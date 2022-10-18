8b7e18dc6e864328f6ed3cb6aa1047173304425f
patiwagura <patiwagura@gmail.com>
2022-10-18 20:03:17 +0300

    Changed  Property configurations

--- src/main/java/com/cobiztech/conference_booking/config/PersistenceConfiguration.java
+++ src/main/java/com/cobiztech/conference_booking/config/PersistenceConfiguration.java
@@ -18,26 +18,26 @@ public class PersistenceConfiguration {
     // Avoid hard-coding sensitive-data e.g passwords into configuration files.
 
     // dependency Injection - inject properties from Environment variables.
-    @Value("${DB_URL}")
-    private String DB_URL;
-
-    @Value("${DB_USERNAME}")
-    private String DB_USERNAME;
-
-    @Value("${DB_PASSWORD}")
-    private String DB_PASSWORD;
-
-    @Bean
-    public DataSource dataSource() {
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url(DB_URL);
-        builder.password(DB_PASSWORD);
-        builder.username(DB_USERNAME);
-        System.out.println("---------My custom datasource :: has been initialized and set.  --- "
-                + DB_URL + " --- "
-                + DB_USERNAME + "  ----"
-                + DB_PASSWORD);
-        return builder.build();
-    }
+//    @Value("${DB_URL}")
+//    private String DB_URL;
+//
+//    @Value("${DB_USERNAME}")
+//    private String DB_USERNAME;
+//
+//    @Value("${DB_PASSWORD}")
+//    private String DB_PASSWORD;
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url(DB_URL);
+//        builder.password(DB_PASSWORD);
+//        builder.username(DB_USERNAME);
+//        System.out.println("---------My custom datasource :: has been initialized and set.  --- "
+//                + DB_URL + " --- "
+//                + DB_USERNAME + "  ----"
+//                + DB_PASSWORD);
+//        return builder.build();
+//    }
 
 }
